ce8a5f0fbccfb24827aed5a6c63404141d95a61c
Anton Shvets m2312516 <antonshvetswork@yandex.ru>
2025-04-26 16:23:15 +0300

    Развернут проект на NextJs

---
+++ src/app/api/user/login/route.ts
@@ -0,0 +1,34 @@
+import { NextResponse } from 'next/server';
+
+// Для POST запросов
+export async function POST(request: Request) {
+  try {
+    const { email, password } = await request.json();
+
+    // Ваша логика аутентификации
+    if (email === "testemail@email.ru" && password === "2222") {
+      return NextResponse.json({ 
+        success: true,
+        message: "Authentication successful"
+      });
+    }
+
+    return NextResponse.json(
+      { success: false, message: "Invalid credentials" },
+      { status: 401 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { success: false, message: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
+
+// Для GET запросов (если нужно)
+export async function GET() {
+  return NextResponse.json(
+    { message: "Method not allowed" },
+    { status: 405 }
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,31 @@
+
+'use client'
+
+export default function Home() {
+
+
+  const auth = async () => {
+    const response = await fetch("/api/user/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email: "testemail@email.ru", password: "2222" }),
+    });
+
+   console.log("response", response);
+  };
+
+
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div className="grid grid-cols-[1fr_3fr_1fr] gap-4">
+        <div className="flex flex-col justify-center">Главная страница</div>
+
+      
+          <button onClick={auth}>Аторизация</button>
+    
+      </div>
+    </div>
+  );
+}
