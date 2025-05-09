a60b935027274637d6bafe0bfe5953710d8b246a
SA <antonshvetswork@yandex.ru>
2025-05-09 21:22:39 +0300

    Обновлены стили полей таблицы

---
+++ src/utils/handleTableRowClass.ts
@@ -0,0 +1,29 @@
+export const handleTableRowClass = (status?: string | undefined | {}) => {
+  let rowClasses =
+    "rounded-lg border cursor-pointer shadow-md shadow-gray-200 dark:bg-gray-800 dark:hover:bg-gray-700 transition-colors cursor-pointer dark:shadow-none";
+
+  // Обработка isActive
+  if (status === "isNotActive") {
+    rowClasses =
+      "rounded-lg bg-rose-50 text-gray-300 hover:bg-rose-100 transition-colors cursor-pointer";
+    return rowClasses;
+  }
+
+  // Обработка статусов
+  switch (status) {
+    case "Завершена":
+      rowClasses =
+        "rounded-lg bg-emerald-50 text-emerald-800 hover:bg-emerald-100 transition-colors cursor-pointer";
+      break;
+    case "Отменена":
+      rowClasses =
+        "rounded-lg bg-amber-50 text-amber-800 hover:bg-amber-100 transition-colors cursor-pointer";
+      break;
+    case "Новая":
+      rowClasses =
+        "rounded-lg bg-[#EFF6FF]  hover:bg-red-100 transition-colors cursor-pointer";
+      break;
+  }
+
+  return rowClasses;
+};
