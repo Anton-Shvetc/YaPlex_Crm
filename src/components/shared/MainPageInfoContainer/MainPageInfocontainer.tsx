2a0e640bc2690721fffcefe185365b46ec4001cb
SA <antonshvetswork@yandex.ru>
2025-05-09 18:29:48 +0300

    Обновлена инофрмация для главной страницы

--- src/components/shared/MainPageInfoContainer/MainPageInfocontainer.tsx
+++ src/components/shared/MainPageInfoContainer/MainPageInfocontainer.tsx
@@ -1,16 +1,28 @@
+import { clsx } from "clsx";
+
+
 interface MainPageInfoProps {
   title: string;
   children?: React.ReactNode;
+  isGrid?: boolean;
 }
 
 export const MainPageInfoContainer: React.FC<MainPageInfoProps> = ({
   title,
   children,
+  isGrid = true,
 }) => (
   <div>
     {title}
 
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 w-full">
+    <div
+      className={clsx(
+        "w-full",
+        isGrid
+          ? "grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4"
+          : "flex flex-col gap-2"
+      )}
+    >
       {children}
     </div>
   </div>
---
+++ src/components/ui/MainPageCards/MainPageDealCard.tsx
@@ -0,0 +1,36 @@
+interface MainPageDealCardI {
+  name: string;
+
+  status?: string;
+}
+
+export const MainPageDealCard: React.FC<MainPageDealCardI> = ({
+  name,
+
+  status,
+}) => {
+  let rowClasses =
+    "rounded-lg border cursor-pointer shadow-md shadow-gray-200 dark:bg-gray-800 dark:hover:bg-gray-700 transition-colors cursor-pointer dark:shadow-none";
+
+  // Обработка статусов
+  switch (status) {
+    case "success":
+      rowClasses =
+        "rounded-lg bg-emerald-50 text-emerald-800 hover:bg-emerald-100 transition-colors cursor-pointer";
+      break;
+    case "warning":
+      rowClasses =
+        "rounded-lg bg-amber-50 text-amber-800 hover:bg-amber-100 transition-colors cursor-pointer";
+      break;
+    case "error":
+      rowClasses =
+        "rounded-lg bg-red-50 text-red-800 hover:bg-red-100 transition-colors cursor-pointer";
+      break;
+  }
+
+  return (
+    <div className={`w-fill ${rowClasses}`}>
+      <div className="p-2 first:rounded-l-lg last:rounded-r-lg">{name}</div>
+    </div>
+  );
+};
